e5687636bbeba47b32fa1ff4639872eb3c7a6740
hlokman <abdallahlokmanhenni@gmail.com>
2023-04-14 00:00:28 +0200

    Make the function case-insensitive

--- RPC.js
+++ RPC.js
@@ -6,26 +6,26 @@ function getComputerChoice() {
 //console.log(getComputerChoice())
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+    if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'paper') {
         return "You Lose! Paper beats Rock";
-    } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'scissors') {
         return "You Win! Rock beats Scissors";
-    } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'rock') {
         return "You Win! Paper beats Rock";
-    } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'scissors') {
         return "You Lose! Scissors beats Paper";
-    } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+    } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'rock') {
         return "You Lose! Rock beats Scissors";
-    } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+    } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'paper') {
         return "You Win! Scissors beats Paper";
-    } else if (playerSelection === computerSelection) {
+    } else if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
         return "It's a tie!";
     } else { 
         return "Please choose between Rock, Paper and Scissors";
     }
 }
 const computerSelection = getComputerChoice();
-const playerSelection = 'Rock';
+const playerSelection = 'paper';
 
 console.log(playRound(playerSelection, computerSelection));
 console.log(computerSelection);
