8d1cdb9139f32089693e9f412bef6650df9a8cc6
hlokman <abdallahlokmanhenni@gmail.com>
2023-04-13 23:48:47 +0200

    Create a function that play a round of RPC
    We create a function with two parameters and make a round of Rock Paper
    Scissors with every possibility.

--- RPC.js
+++ RPC.js
@@ -2,7 +2,30 @@ function getComputerChoice() {
     let myArray=['Rock','Paper','Scissors'];
     let random= Math.floor(Math.random() * myArray.length) ;
     return randomChoice= myArray[random];
-    console.log(random);
 } 
+//console.log(getComputerChoice())
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+        return "You Lose! Paper beats Rock";
+    } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+        return "You Win! Rock beats Scissors";
+    } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+        return "You Win! Paper beats Rock";
+    } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+        return "You Lose! Scissors beats Paper";
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+        return "You Lose! Rock beats Scissors";
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        return "You Win! Scissors beats Paper";
+    } else if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else { 
+        return "Please choose between Rock, Paper and Scissors";
+    }
+}
+const computerSelection = getComputerChoice();
+const playerSelection = 'Rock';
+
+console.log(playRound(playerSelection, computerSelection));
+console.log(computerSelection);
